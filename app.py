7954020f17f9d3f605f45ef55abe10c7bbeb457f
jsparo9 <sudhanshu.209303249@muj.manipal.edu>
2023-01-29 17:44:32 +0530

    "Added requirements.txt"

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ if __name__ == '__main__':
     # Serve the app with gevent
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
-    app.run()
+    app.run(debug=False,host="0.0.0.0")
